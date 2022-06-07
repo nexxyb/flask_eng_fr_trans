af951323d59de04554feb3a3e71200c51cbb136e
nexxyb <nexxyb@gmail.com>
2022-06-07 09:49:36 +0100

    File Updated

--- final_project/tests.py
+++ final_project/tests.py
@@ -4,11 +4,11 @@ from machinetranslation import translator
 class test_translator(unittest.TestCase):
     def test_english_to_french(self):
         self.assertEqual(translator.english_to_french('Hello'), 'Bonjour')
-        self.assertNotEqual(translator.english_to_french(''),'home')
+        self.assertNotEqual(translator.english_to_french('help'),'home')
 
     def test_french_to_english(self):
         self.assertEqual(translator.french_to_english('Bonjour'), 'Hello')
-        self.assertNotEqual(translator.french_to_english(''),'home')
+        self.assertNotEqual(translator.french_to_english('oui'),'home')
 
 if __name__ == '__main__':
     unittest.main()
